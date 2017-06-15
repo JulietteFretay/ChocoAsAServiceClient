f5dc5cff013f4dd2c1121bf36f59d9cfaf8113ff
Théo Andrieux <tandrieux@icloud.com>
2017-06-15 18:22:04 +0200

    Envoi du probleme et retour de l'id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component, NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 export class User {
   id: number;
   name: string;
 }
 
-@NgModule({
-  imports: [
-    HttpModule,
-  ],
-})
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,25 +16,15 @@ export class User {
 export class AppComponent {
   title = 'Choco As A Service';
 
-  serverUrl = 'localhost:4567/';
+  serverUrl = 'http://localhost:4567';
 
-  problemFile;
+  problem;
   problemId;
 
-  submitProblem() {
-    if (this.problemFile) {
-      const reader = new FileReader();
-      reader.readAsText(this.problemFile);
-      reader.onload = function(){
-        return;
-      };
-    }
-  }
+  constructor(private http: Http) { }
 
-  loadFile(files) {
-    if (files) {
-      this.problemFile = files[0];
-    }
+  loadProblem() {
+    this.http.post(this.serverUrl + '/problem', this.problem).toPromise().then(res => this.problemId = res.text());
   }
 
 }
